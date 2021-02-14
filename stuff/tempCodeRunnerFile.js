f9f1b894f94964d4ec3208977d539036118b14bd
whyroland <42717361+whyroland@users.noreply.github.com>
2021-02-14 08:53:34 -0500

    cleaning

--- stuff/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-indbodygreen-res.cloudinary.com/images/w_767,q_auto:eco,f_auto,fl_lossy/org/zf7a8y99cjt0cpxj9/is-it-acne-or-rosacea-how-to-spot-the-difference-according-to-a-derm.jpg
-        },
-        metadata,
-        (err, response) => {
